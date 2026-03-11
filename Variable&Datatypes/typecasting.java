695802a92f45b771df88953b60a781be1135cf36
Ayan <ansariayan6162@gmail.com>
2026-03-11 17:33:21 +0530

    Day 1 - Java basics, datatypes, typecasting, input

---
+++ Variable&Datatypes/typecasting.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+public class typecasting {
+
+    public static void main(String args[]){
+
+        Scanner  sc = new Scanner(System.in);
+       //     int number = sc.nextInt();
+        // System.out.println(number);
+
+
+float marks = 9.8f;
+int marks2 = (int)marks;
+
+System.out.println(marks);
+
+ //char ch = 'a';
+ //int number = ch;
+ //System.out.print(number);
+
+
+    }
+
+    
+}
