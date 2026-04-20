c7b1f2d37e02aa9ef71e8e34bc55e83f27b350fc
Ayan <ansariayan6162@gmail.com>
2026-04-21 01:24:10 +0530

    Add operators folder with all files

---
+++ operators/relational.java
@@ -0,0 +1,22 @@
+public class relational {
+    
+
+public static void main(String argrs[]){
+
+int a =10;
+ int b =20;
+
+System.out.println(a==b);
+//LOGICAL OPERATORS
+//n   && --------->LOGICAL and
+//    || ----------->logical or  
+//     ! ----------->logical  not 
+
+System.out.println((3>2)&&(5>2));
+System.out.println((3>2)||(9<1));
+System.out.println(!(6>2));
+
+
+}
+
+}
