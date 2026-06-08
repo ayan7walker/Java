6300c3de2466fc1b19d339d62203bae2ec0bf5f2
Ayan <ansariayan6162@gmail.com>
2026-06-08 21:48:16 +0530

    max sub array brute force

---
+++ array/maxsubarray.java
@@ -0,0 +1,36 @@
+package array;
+
+public class maxsubarray {
+
+    public static int maxSubarraySum(int[] numbers) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                currSum = 0;
+
+                for (int k = start; k <= end; k++) {
+                    currSum += numbers[k]; // ✅ Bug 1 fixed
+                }
+
+                System.out.println(currSum);
+
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+
+        System.out.println("max sum = " + maxSum);
+        return maxSum; // ✅ Bug 2 fixed
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        maxSubarraySum(numbers);
+    }
+}
